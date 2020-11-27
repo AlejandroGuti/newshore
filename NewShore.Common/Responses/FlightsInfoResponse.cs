939c608423f3e495ec8ea9f5a76c194b34417ac8
alegut123 <alejandro.gutierrez@rauxmedia.com>
2020-11-27 13:49:16 -0500

    SearchDestination

--- NewShore.Common/Responses/FlightsInfoResponse.cs
+++ NewShore.Common/Responses/FlightsInfoResponse.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace NewShore.Common.Responses
 {
     public class FlightsInfoResponse
     {
+
+        public string DepartureDate { get; set; }
+        public string DepartureStation { get; set; }
+        public string ArrivalStation { get; set; }
+        public string FlightNumber { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
 
     }
 }
